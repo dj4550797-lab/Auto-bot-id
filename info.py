cf9d7ff1324689ac080d540e0d5ba8fd79c1f9df
dj4550797-lab <dj4550797@gmail.com>
2026-04-07 10:37:05 +0530

    Fix environment variable default values in info.py

--- info.py
+++ info.py
@@ -3,12 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.environ.get("API_ID", 0))
+API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split(",") if x.strip()]
-
-MONGO_URI = os.environ.get("MONGO_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "FlixoraBotDB")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
